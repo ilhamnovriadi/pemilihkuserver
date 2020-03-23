11933bfad76f763f6254d54da29e4d7a8893b2a2
Ilham Novriadi <ilhamnovriadi@Ilhams-MacBook-Pro.local>
2020-03-23 23:04:32 +0700

    konek mongodb

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+
+    console.log("MongoDB konek gays...");
+  } catch (err) {
+    console.error(err.massage);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
